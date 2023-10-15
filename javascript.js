55519ff95de05a2501e68817ef31cdb18af41951
CarlosAle26 <carlinho.dz@outlook.com>
2023-10-15 09:23:37 -0300

    Lista de Tarefas

---
+++ javascript.js
@@ -0,0 +1,24 @@
+/*VARIÁVEIS
+
+ -let / const / var (descontinuado, não é usado mais)
+    const -> Toda vez que eu crio uma variável com const, eu não posso atribuir um novo valor depois;
+    let -> Posso alterar o valor quantas vezes eu quiser;
+*/
+
+
+/*IF (SE) / ELSE 
+   -Nos ajuda a ensinar nosso código a tomar decisões
+    if (condição, se for verdadeiro){
+        faz isso
+    }
+
+    else {
+        se o if for falso, faço isso!
+    }
+    Funções 
+      -São um trecho de código, que só é executado, quando é chamado
+
+
+    Laços
+      -ForEach
+   */
